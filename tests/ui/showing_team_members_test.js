725aabf4afaac1837316f75d81ab9db259326223
Diarmaid McGowan and Fabio Mangione <pair+dmcgc+fmang@allstate.com>
2016-02-22 12:06:48 +0000

    Reverted temp change to ui test

--- tests/ui/showing_team_members_test.js
+++ tests/ui/showing_team_members_test.js
@@ -4,7 +4,7 @@ Scenario('Index page shows Roma players', function (I) {
 
   I.amOnPage('/');
 
-  //I.seeInTitle('A.S.Roma players championship 2000/2001');
+  I.seeInTitle('A.S.Roma players championship 2000/2001');
 
   I.see('Francesco Totti');
   I.see('Vincenzo Montella');
